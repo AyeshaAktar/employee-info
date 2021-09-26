dc66329d19ed6e5806cbce50e6dfc5e3f8a3b27b
Ayesha Akter <ayeshaaktar491@gmail.com>
2021-09-26 14:11:41 +0600

    employees.JSON

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Web Developer Team</h1>
+      <p>Employee selection list for new project.</p>
+      <h2>Total Budget: 150 Million</h2>
+    </div>
+  );
+};
+
+export default Header;
--- src/data/employee.js
+++ src/data/employee.js
@@ -1,11 +1,152 @@
 const employee = [
   {
-    name: "Ayesha",
-    id: 2191081045,
+    name: "David", //done
+    id: 1,
+    employeeId: 219108001,
+    age: 36,
+    workAs: "Manager",
+    country: "BD",
+    salary: 200000,
+    img: "https://images.unsplash.com/photo-1560250097-0b93528c311a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8ZW1wbG95ZWV8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80",
+  },
+  {
+    name: "Lisa", //done female
+    id: 2,
+    employeeId: 219108002,
+    age: 30,
+    workAs: "Full Stack Developer",
+    country: "BD",
+    salary: 150000,
+    img: "https://thumbs.dreamstime.com/b/satisfied-office-employee-looking-camera-thumbs-up-you-108222007.jpg",
+  },
+  {
+    name: "Denzel", //done male
+    id: 3,
+    employeeId: 219108003,
+    age: 29,
+    workAs: "Full Stack Developer",
+    country: "BD",
+    salary: 150000,
+    img: "https://image.shutterstock.com/image-photo/close-handsome-businessman-smiling-camera-260nw-320660525.jpg",
+  },
+  {
+    name: "Jhony", //done male
+    id: 4,
+    employeeId: 219108004,
+    age: 31,
+    workAs: "Full Stack Developer",
+    country: "BD",
+    salary: 150000,
+    img: "https://image.shutterstock.com/image-photo/portrait-cheerful-young-businessman-crossed-260nw-251771635.jpg",
+  },
+  {
+    name: "Jennifer Lawrence", //done female
+    id: 5,
+    employeeId: 219108005,
+    age: 30,
+    workAs: "Full Stack Developer",
+    country: "BD",
+    salary: 150000,
+    img: "https://dm0qx8t0i9gc9.cloudfront.net/thumbnails/image/rDtN98Qoishumwih/graphicstock-portrait-of-confident-employee-working-with-laptop_SgxsQYgxW_thumb.jpg",
+  },
+  {
+    name: "Emma Watson", //done female
+    id: 6,
+    employeeId: 219108006,
+    age: 27,
+    workAs: "Front End Developer",
+    country: "BD",
+    salary: 80000,
+    img: "https://image.shutterstock.com/image-photo/passport-picture-businesswoman-brown-hair-260nw-250775908.jpg",
+  },
+  {
+    name: "Keanu", //done male
+    id: 7,
+    employeeId: 219108007,
+    age: 28,
+    workAs: "Front End Developer",
+    country: "BD",
+    salary: 80000,
+    img: "https://dm0qx8t0i9gc9.cloudfront.net/thumbnails/image/rDtN98Qoishumwih/WM2A7258-1507_thumb.jpg",
+  },
+  {
+    name: "Anne", //done female
+    id: 8,
+    employeeId: 219108008,
+    age: 27,
+    workAs: "Front End Developer",
+    country: "BD",
+    salary: 80000,
+    img: "https://www.ecumen.org/wp-content/uploads/2014/12/Langan_Gabbi_LR.jpg",
+  },
+  {
+    name: "Sean", //done male
+    id: 9,
+    employeeId: 219108009,
+    age: 28,
+    workAs: "Back End Developer",
+    country: "BD",
+    salary: 90000,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTo3E_eLS5x5hU4yFpNi7MimMSIzjRi5t2ZGA&usqp=CAU",
+  },
+  {
+    name: "Jacqueline", //female
+    id: 10,
+    employeeId: 219108010,
+    age: 26,
+    workAs: "Back End Developer",
+    country: "BD",
+    salary: 90000,
+    img: "https://www.faegredrinker.com/-/media/images/professionals/v/emmavennesson.jpg?mw=300&hash=2B452216772AAF12884E9157C09C2423832DF0FB",
+  },
+  {
+    name: "Richard", //male
+    id: 11,
+    employeeId: 219108011,
+    age: 27,
+    workAs: "Back End Developer",
+    country: "BD",
+    salary: 90000,
+    img: "https://res.cloudinary.com/crunchbase-production/image/upload/c_thumb,h_256,w_256,f_auto,g_faces,z_0.7,q_auto:eco,dpr_1/lzywd9je1m9snyez8zjc",
+  },
+  {
+    name: "Natalie Portman", //female
+    id: 12,
+    employeeId: 219108012,
+    age: 27,
+    workAs: "Designer",
+    country: "BD",
+    salary: 70000,
+    img: "https://thumbs.dreamstime.com/b/portrait-happy-office-employee-working-laptop-looking-camera-109745321.jpg",
+  },
+  {
+    name: "Edward", //male
+    id: 13,
+    employeeId: 219108013,
+    age: 29,
+    workAs: "Designer",
+    country: "BD",
+    salary: 70000,
+    img: "https://image.shutterstock.com/image-photo/happy-handsome-business-man-holding-260nw-786682531.jpg",
+  },
+  {
+    name: "Megan fox", //female
+    id: 14,
+    employeeId: 219108014,
+    age: 24,
+    workAs: "Designer",
+    country: "BD",
+    salary: 70000,
+    img: "https://thumbs.dreamstime.com/b/happy-young-business-woman-employee-using-computer-working-office-chatting-internet-communicating-online-workplace-smiling-153902635.jpg",
+  },
+  {
+    name: "Ayesha", //done
+    id: 15,
+    employeeId: 219108015,
     age: 24,
     workAs: "Junior Developer",
     country: "BD",
     salary: 20000,
-    img: "https://thumbs.dreamstime.com/b/satisfied-office-employee-looking-camera-thumbs-up-you-108222007.jpg",
+    img: "https://www.mindtickle.com/wp-content/uploads/2013/10/remote-employee-mindtickle-new-employee-orientation.jpg",
   },
 ];
