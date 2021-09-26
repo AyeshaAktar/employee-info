6ab50f86f145fd8134938304a74ed012293009cc
Ayesha Akter <ayeshaaktar491@gmail.com>
2021-09-26 10:52:20 +0600

    add

---
+++ src/data/employee.js
@@ -0,0 +1,11 @@
+const employee = [
+  {
+    name: "Ayesha",
+    id: 2191081045,
+    age: 24,
+    workAs: "Junior Developer",
+    country: "BD",
+    salary: 20000,
+    img: "https://thumbs.dreamstime.com/b/satisfied-office-employee-looking-camera-thumbs-up-you-108222007.jpg",
+  },
+];
